ab6c483086d7449fb0b6e297f03c84ad802c476d
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-17 17:31:11 -0500

    Var, Let and Const

---
+++ 00 - Nivelación/js/variables.js
@@ -0,0 +1,34 @@
+//Var
+
+// document.write(nombre); //undefined
+
+// var nombre = "Flor";
+
+// saludo();
+
+// var saludo = function() {
+//     document.write("Hola mundo")
+// }
+
+
+//Let
+
+// document.write(nombre); //No trae nada
+// let nombre = "Flor";
+
+// let nombre = "Flor";
+// document.write(nombre); //Flor
+
+// let nombre = "Flor";
+// nombre = "Maria"
+// document.write(nombre); //Maria
+
+
+//Por el anterior y último ejemplo, se opta por Const
+
+// const nombre = "Flor";
+// nombre = "Maria"
+// document.write(nombre); //No es permitido
+
+const nombre = "Flor";
+document.write(nombre); //Flor
