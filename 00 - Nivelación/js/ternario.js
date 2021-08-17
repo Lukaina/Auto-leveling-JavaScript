95c6c8d2afa4ae58c9dcc0f8a63b59b56e42f002
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-17 18:22:19 -0500

    Ternary Operator

---
+++ 00 - Nivelación/js/ternario.js
@@ -0,0 +1,26 @@
+// const cuenta = 10;
+// let mensaje;
+
+// if (cuenta < 0) {
+//     mensaje = "No tienes saldo"
+// } else {
+//     mensaje = "Tienes saldo"
+// }
+
+// document.write(mensaje)
+
+
+//Con Operador Ternario
+
+// const cuenta = -10;
+
+// const mensaje = cuenta < 0;
+
+// document.write(mensaje) //true
+// ---
+const cuenta = -10;
+
+const mensaje = cuenta < 0 ? "No tienes saldo" : "Tienes saldo";
+
+document.write(mensaje) 
+
