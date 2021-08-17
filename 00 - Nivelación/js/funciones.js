e45265ce772815a47a4f5031fc36b1259d494d07
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-17 17:53:58 -0500

    Arrow Function

---
+++ 00 - Nivelación/js/funciones.js
@@ -0,0 +1,29 @@
+// function suma(a, b) {
+//     // document.write(a + b)
+//     return a + b;
+// }
+
+// // suma(2, 2) //No se puede olvidar invocar la función
+// const resultado = suma(2, 2);
+
+// document.write(resultado);
+
+
+//Arrow function
+
+// const suma = (a, b) => {
+//     return a + b;
+// }
+
+// const resultado = suma(2, 2);
+
+// document.write(resultado);
+
+
+//Se puede simplificar aún más
+
+const suma = (a, b) => a + b; //Se quitan las llaves y ya no es necesario el return
+
+const resultado = suma(2, 2);
+
+document.write(resultado);
