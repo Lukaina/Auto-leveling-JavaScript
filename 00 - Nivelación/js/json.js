01d3c0d443dc3cc32c2fc2a2cd2cfa2a128d1990
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-17 18:46:09 -0500

    JSON

---
+++ 00 - Nivelación/js/json.js
@@ -0,0 +1,23 @@
+//Antes
+
+// var animal = {
+//     color: "amarillo",
+//     patas: 4,
+//     suave: false,
+//     setTextura: function() {
+//         this.suave = true
+//     }
+// }
+
+// console.log(animal);
+
+
+//Ahora
+
+const usuario = { //Igual a un objeto
+    nombre: "Julia",
+    edad: 3,
+    personalidad: "Alegre"
+}
+
+document.write(usuario.edad)
