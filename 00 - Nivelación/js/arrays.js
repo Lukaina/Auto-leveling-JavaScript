f96c5490dc3781298160e77e464d168fed71880b
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-17 19:54:51 -0500

    Array Destructuring

--- 00 - Nivelación/js/arrays.js
+++ 00 - Nivelación/js/arrays.js
@@ -1,4 +1,4 @@
-const numeros = [1, 2, 3, 4];
+// const numeros = [1, 2, 3, 4];
 
 // document.write(numeros);
 
@@ -11,4 +11,18 @@ const numeros = [1, 2, 3, 4];
 // Pero existe el método Map que recibe un callback. 
 //Podría ir el índice si lo queremos usar
 
-numeros.map((numero) => document.write(numero))
+// numeros.map((numero) => document.write(numero))
+
+// ---
+
+//Array Destructuring
+
+// const usuarios = ["Pepito", "Andy", "Sami", "Cami"]
+
+// document.write(usuarios)
+
+//Funciona de manera posicional
+
+const [, usuario2 ,] = ["Pepito", "Andy", "Sami", "Cami"]
+
+document.write(usuario2)
