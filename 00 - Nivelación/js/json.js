12bf59ab16f52e17f3d343ba89b825aa30182fe2
Verónica Alcaraz <60724393+Lukaina@users.noreply.github.com>
2021-08-17 19:14:04 -0500

    Object Destructuring

--- 00 - Nivelación/js/json.js
+++ 00 - Nivelación/js/json.js
@@ -14,10 +14,38 @@
 
 //Ahora
 
-const usuario = { //Igual a un objeto
-    nombre: "Julia",
-    edad: 3,
-    personalidad: "Alegre"
-}
+// const usuario = { //Igual a un objeto
+//     nombre: "Julia",
+//     edad: 3,
+//     personalidad: "Alegre"
+// }
 
-document.write(usuario.edad)
+// document.write(usuario.edad)
+
+
+//Object Destructuring
+
+// const usuario = { //Igual a un objeto
+//         nombre: "Julia",
+//         edad: 3,
+//         personalidad: "Alegre"
+//     }
+    
+// document.write(usuario.nombre)
+// document.write(usuario.edad)
+// document.write(usuario.personalidad)
+
+//Para no tener que escribir usuario. una y otra vez, usamos el destructuring
+
+const {nombre, edad, personalidad} = { 
+        nombre: "Julia",
+        edad: 3,
+        personalidad: "Alegre"
+    }
+    
+// document.write(nombre)
+// document.write(edad)
+// document.write(personalidad)
+document.write(nombre, edad, personalidad)
+
+    
